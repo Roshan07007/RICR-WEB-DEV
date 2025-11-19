64980e485c562f47aafd3a3e8a9c1bdcc2073ae4
roshan07007 <malviyakalar@gmail.com>
2025-11-19 18:21:03 +0530

    feat: Enhance RICR Enquiry Page with new form fields and styling
    - Updated index.html to include additional input fields for user details such as branch and course interests.
    - Improved layout and styling in style.css for better user experience.
    - Added functionality in script.js to handle new input fields and reset form after submission.
    - Included a call icon in the header for better accessibility.

---
+++ java-script/RICR-Enquiry-Page/script.js
@@ -0,0 +1,47 @@
+function submit() {
+  console.log("click to submit");
+
+  const a = document.getElementById("username").value;
+  const b = document.getElementById("usernumber").value;
+  const c = document.getElementById("useremail").value;
+  const d = document.getElementById("userqualification").value;
+  const e = document.getElementById("usercollage").value;
+  const f = document.getElementById("useryear").value;
+  const g = document.getElementById("userbranch").value;
+  const h = document.getElementById("usersubject").value;
+  const i = document.getElementById("usersubjects").value;
+  const j = document.getElementById("Business").value;
+  const k = document.getElementById("java").value;
+  const l = document.getElementById("pythan").value;
+  const m = document.getElementById("datascience").value;
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+  console.log(e);
+  console.log(f);
+  console.log(g);
+  console.log(h);
+  console.log(i);
+  console.log(j);
+  console.log(k);
+  console.log(l);
+  console.log(m);
+
+  alert("done submit");
+
+  document.getElementById("username").value = "";
+  document.getElementById("usernumber").value = "";
+  document.getElementById("useremail").value = "";
+  document.getElementById("userqualification").value = "";
+  document.getElementById("usercollage").value = " ";
+  document.getElementById("useryear").value = " ";
+  document.getElementById("userbranch").value = "";
+  document.getElementById("usersubject").value = " ";
+  document.getElementById("usersubjects").value = " ";
+  document.getElementById("Business").value = "";
+  document.getElementById("java").value = " ";
+  document.getElementById("pythan").value = " ";
+  document.getElementById("datascience").value = "";
+}

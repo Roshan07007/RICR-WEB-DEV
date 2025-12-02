52580cadef8d75a1ede260870fd595a64db66537
roshan07007 <malviyakalar@gmail.com>
2025-12-02 19:59:13 +0530

    feat: Enhance image editor with filter controls and add To Do list functionality

--- imageEditor/script.js
+++ imageEditor/script.js
@@ -8,3 +8,64 @@ function upimage() {
   img.src = fileURL;
   document.getElementById("labelID").style.display = "none";
 }
+
+let b = 1,
+  c = 1,
+  g = 0,
+  s = 0,
+  i = 0;
+
+function applyFilter() {
+  document.getElementById("image").style.filter = ` brightness(${b}) 
+                                                    contrast(${c}) 
+                                                    grayscale(${g}%) 
+                                                    sepia(${s}%)
+                                                    invert(${i}%)`;
+}
+
+function changebrightness() {
+  const value = document.getElementById("brightness").value;
+  b = (value * 2) / 100;
+  applyFilter();
+}
+
+function Contrast() {
+  const value = document.getElementById("Contrast").value;
+
+  b = (value * 2) / 100;
+  applyFilter();
+}
+
+function Grayscale() {
+  const value = document.getElementById("Grayscale").value;
+  b = (value * 2) / 100;
+  applyFilter();
+}
+
+function Sepia() {
+  const value = document.getElementById("Sepia").value;
+  b = (value * 2) / 100;
+  applyFilter();
+}
+
+function Saturate() {
+  const value = document.getElementById("Saturate").value;
+  b = (value * 2) / 100;
+  applyFilter();
+}
+
+function rotate() {
+  const value = document.getElementById("rotate").value;
+  b = (value * 2) / 100;
+  applyFilter();
+}
+function Blur() {
+  const value = document.getElementById("Blur").value;
+  b = (value * 2) / 100;
+  applyFilter();
+}
+function Invert() {
+  const value = document.getElementById("Saturate").value;
+  b = (value * 2) / 100;
+  applyFilter();
+}

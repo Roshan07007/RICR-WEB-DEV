3a9b986ab0142e6e0fbede4effb054c645dcd716
roshan07007 <malviyakalar@gmail.com>
2025-12-05 20:28:20 +0530

    new commit

--- java-script/Event/script.js
+++ java-script/Event/script.js
@@ -25,3 +25,59 @@ function smartBulb() {
     document.getElementById("smartbtn").innerText = "On";
   }
 }
+
+
+
+const colorBulb = document.getElementById("colorBulb");
+
+document.getElementById("red").addEventListener("mouseenter", () => {
+  colorBulb.style.backgroundColor = "red";
+  colorBulb.style.color = "white";
+});
+document.getElementById("red").addEventListener("mouseleave", () => {
+  colorBulb.style.backgroundColor = "inherit";
+  colorBulb.style.color = "black";
+});
+
+document.getElementById("blue").addEventListener("mouseenter", () => {
+  colorBulb.style.backgroundColor = "blue";
+  colorBulb.style.color = "white";
+});
+document.getElementById("blue").addEventListener("mouseleave", () => {
+  colorBulb.style.backgroundColor = "inherit";
+  colorBulb.style.color = "black";
+});
+
+document.getElementById("yellow").addEventListener("mouseenter", () => {
+  colorBulb.style.backgroundColor = "yellow";
+});
+document.getElementById("yellow").addEventListener("mouseleave", () => {
+  colorBulb.style.backgroundColor = "inherit";
+  colorBulb.style.color = "black";
+});
+
+document.getElementById("green").addEventListener("mouseenter", () => {
+  colorBulb.style.backgroundColor = "green";
+  colorBulb.style.color = "white";
+});
+document.getElementById("green").addEventListener("mouseleave", () => {
+  colorBulb.style.backgroundColor = "inherit";
+  colorBulb.style.color = "black";
+});
+
+document.getElementById("pink").addEventListener("mouseenter", () => {
+  colorBulb.style.backgroundColor = "pink";
+  colorBulb.style.color = "white";
+});
+document.getElementById("pink").addEventListener("mouseleave", () => {
+  colorBulb.style.backgroundColor = "inherit";
+  colorBulb.style.color = "black";
+});
+document.getElementById("orange").addEventListener("mouseenter", () => {
+  colorBulb.style.backgroundColor = "orange";
+  colorBulb.style.color = "white";
+});
+document.getElementById("orange").addEventListener("mouseleave", () => {
+  colorBulb.style.backgroundColor = "inherit";
+  colorBulb.style.color = "black";
+});

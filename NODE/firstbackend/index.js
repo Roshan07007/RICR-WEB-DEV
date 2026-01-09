c01fab54a5023f0a042c8834eb68eadd9db214b7
Roshan07007 <malviyakalar@gmail.com>
2026-01-09 17:59:25 +0530

    fix: correct console log message for server start

--- NODE/firstbackend/index.js
+++ NODE/firstbackend/index.js
@@ -12,5 +12,5 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server started  at  port ", port);
+  console.log("Server started  at  port a ", port);
 });

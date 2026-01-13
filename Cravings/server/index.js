92f4e87d46a92bfedeb5945fee1eb514d90ab900
Roshan07007 <malviyakalar@gmail.com>
2026-01-13 18:28:11 +0530

    feat: Initialize server with user authentication
    - Added package.json for server configuration and dependencies.
    - Implemented MongoDB connection in db.js.
    - Created authController.js for user registration, login, and logout functionalities.
    - Defined user model in userModel.js with necessary fields.
    - Set up authRouter.js for handling authentication routes.
    - Added user registration, login, logout, and update functionalities in myController.js.
    - Created myRouter.js to manage user-related routes.

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -1,21 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 import connectDB from "./src/config/db.js";
-import AuthRouter from "./src/routers/myRouter.js";
+import AuthRouter from "./src/routers/authRouter.js";
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/auth", AuthRouter);
 
-
-app.get("/", (req, res) => {  
-  console.log("Server is Running");
-  res.json({ message: "Server is Running Sucessfully" });
-}); 
+app.get("/", (req, res) => {
+  console.log("Server is Working");
+});
 
 app.use((err, req, res, next) => {
   const ErrorMessage = err.message || "Internal Server Error";
@@ -26,6 +27,6 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server Started at port ", port);
+  console.log("Server Started at Port: ", port);
   connectDB();
 });
